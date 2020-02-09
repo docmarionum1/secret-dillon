efee5c06b915d52d94908514cc86a3eef8e49ddf
Glitch (hello-express) <none>
2020-02-09 21:03:36 +0000

    🏗💼 Checkpoint ./app.js:11230593/2236

--- app.js
+++ app.js
@@ -10,18 +10,57 @@ const games = {};
 
 
 
-
+async function newGame(channel, context) {
+  console.log(context);
+  const members = [];
+  while (true) {
+    const members = await app.client.conversations.members({
+    token: context.botToken,
+    channel: channel,
+      netx
+  });
+  }
+  
+  
+  console.log(members);
+  // await app.client.chat.postMessage({
+  //   token: context.botToken,
+  //   channel: message.user,
+  //   text: "You are a dillon"
+  // });
+}
 
 app.message('new', async ({message, context, say}) => {
   if (message.channel in games) {
-    say("Game already in progress!")
+    app.client.chat.postEphemeral({
+      token: context.botToken,
+      channel: message.channel,
+      user: message.user,
+      "blocks": [
+        {
+          "type": "section",
+          "text": {
+            "type": "mrkdwn",
+            "text": "A game is already in progress - are you sure you want to end the current game and start a new one?"
+          },
+          "accessory": {
+            "type": "button",
+            "text": {
+              "type": "plain_text",
+              "text": "New Game",
+              "emoji": true
+            },
+            "value": "new_game",
+                    "style": "danger"
+          }
+        }
+      ]
+    });
+    return;
+  } else {
+    newGame(message.channel, context);
   }
-  console.log(message);
-  await app.client.chat.postMessage({
-    token: context.botToken,
-    channel: message.user,
-    text: "You are a dillon"
-  });
+
 });
 
 
